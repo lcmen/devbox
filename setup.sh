90a6264c60f004b93967049d5f3191a068748554
Lucas Mendelowski <lucas@mendelowski.com>
2020-08-21 21:16:38 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 read -s -p "[sudo] sudo password for $(whoami): " pass
 
-echo "${pass}" | sudo -S apt update
-echo "${pass}" | sudo -S apt install software-properties-common
 echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
-echo "${pass}" | sudo -S apt install ansible
+echo "${pass}" | sudo -S apt update
+echo "${pass}" | sudo -S apt install --yes ansible software-properties-common
 
 ansible-playbook -i inventories/local site.yml
