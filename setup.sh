c724e8ca25550b281c969cf35066d1db5edcb9e6
Lucas Mendelowski <lucas@mendelowski.com>
2020-08-22 09:50:50 +0200

    Update playbook

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
 echo "${pass}" | sudo -S apt update
 echo "${pass}" | sudo -S apt install --yes ansible software-properties-common
 
+echo "Clonning playbook into ${PWD}/devbox"
+git clone https://github.com/lowski/devbox.git && cd devbox
+
+echo "Provisioning"
 ansible-playbook -i inventories/local site.yml
