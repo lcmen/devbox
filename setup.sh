adf89e046ad1a6beb075d9eeb5684a9b27c8c39f
Lucas Mendelowski <lucas@mendelowski.com>
2020-08-23 20:49:32 +0200

    Fix asdf installation

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@
 
 read -r -s -p "[sudo] sudo password for $(whoami): " pass
 
+echo "${pass}" | sudo -S apt install --yes software-properties-common
 echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
 echo "${pass}" | sudo -S apt update
-echo "${pass}" | sudo -S apt install --yes ansible software-properties-common
+echo "${pass}" | sudo -S apt install --yes ansible
 
-echo "Clonning playbook into ${PWD}/devbox"
-git clone https://github.com/lowski/devbox.git && cd devbox
+# if [ ! -d "${PWD}/devbox" ]; then
+#   echo "Clonning playbook into ${PWD}/devbox"
+#   git clone https://github.com/lowski/devbox.git && cd devbox
+# fi
 
 echo "Provisioning"
 ansible-playbook -i inventories/local site.yml
