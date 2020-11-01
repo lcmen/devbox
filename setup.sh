77b716d18654a366f9ec9837ef4696d2cbbe2603
Lucas Mendelowski <lucas@mendelowski.com>
2020-11-01 09:33:52 +0100

    Add customization role and switch to Python 3

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ read -r -s -p "[sudo] sudo password for $(whoami): " pass
 echo "${pass}" | sudo -S apt install --yes software-properties-common
 echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
 echo "${pass}" | sudo -S apt update
-echo "${pass}" | sudo -S apt install --yes ansible
+echo "${pass}" | sudo -S apt install --yes -–no-install-recommends ansible python3-pip
+pip3 install -r requirements.txt
 
 echo "Provisioning"
 ansible-playbook -i inventories/local site.yml
