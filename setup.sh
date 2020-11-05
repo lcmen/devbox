34a55a5e3173afd0624fba0036b2c11f7363eb3d
Lucas Mendelowski <lucas@mendelowski.com>
2020-11-05 19:11:15 +0100

    Small cleanup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 read -r -s -p "[sudo] sudo password for $(whoami): " pass
 
-echo "${pass}" | sudo -S apt install --yes software-properties-common
+echo "Installinb Ansible and its dependencies"
+echo "${pass}" | sudo -S apt install --yes --no-install-recommends software-properties-common
 echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
-echo "${pass}" | sudo -S apt update
-echo "${pass}" | sudo -S apt install --yes -–no-install-recommends ansible python3-pip
+echo "${pass}" | sudo -S apt install --yes --no-install-recommends ansible python3-pip
 pip3 install -r requirements.txt
 
 echo "Provisioning"
