6f8197641884cd503ae4fee8102cd2f5a3b806c9
Lucas Mendelowski <lucas@mendelowski.com>
2021-01-01 19:04:25 +0100

    Switch to ansible from PIP

--- setup.sh
+++ setup.sh
@@ -3,13 +3,11 @@
 read -r -s -p "[sudo] sudo password for $(whoami): " pass
 
 echo "Installinb Ansible and its dependencies"
-echo "${pass}" | sudo -S apt install --yes --no-install-recommends software-properties-common
-echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
-echo "${pass}" | sudo -S apt install --yes --no-install-recommends ansible python3-pip
+echo "${pass}" | sudo -S apt install --yes --no-install-recommends python3-pip software-properties-common
 pip3 install -r requirements.txt
 
 echo "Provisioning"
-ansible-playbook -i inventories/local site.yml
+~/.local/bin/ansible-playbook -i inventories/local site.yml
 
 echo "Cleaning up"
 echo "${pass}" | sudo -S apt autoremove
