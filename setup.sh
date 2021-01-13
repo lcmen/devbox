a2a984f4bc9467fbd84b8a429f4c1a38b4dba6da
Lucas Mendelowski <lucas@mendelowski.com>
2021-01-13 17:26:31 +0100

    Update for Elementary

--- setup.sh
+++ setup.sh
@@ -2,15 +2,16 @@
 
 read -r -s -p "[sudo] sudo password for $(whoami): " pass
 
-echo "Installinb Ansible and its dependencies"
-echo "${pass}" | sudo -S apt install --yes --no-install-recommends \
-  python3-pip \
-  python3-setuptools \
-  software-properties-common
+echo ""
+echo "Installing Ansible and its dependencies"
+echo "${pass}" | sudo -S apt-add-repository --yes --update ppa:ansible/ansible
+echo "${pass}" | sudo -S apt install --yes --no-install-recommends ansible python3-pip software-properties-common
+
+echo "Installing Ansible modules"
 pip3 install -r requirements.txt
 
 echo "Provisioning"
-~/.local/bin/ansible-playbook -i inventories/local site.yml
+ansible-playbook -i inventories/local site.yml
 
 echo "Cleaning up"
 echo "${pass}" | sudo -S apt autoremove
